0c898cdd1c6b4f621a010ece3b609c0c155927c0
Neha <nehaprajapati1150@gmail.com>
2022-01-20 14:05:30 +0530

    styled-components advance

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,11 +1,10 @@
 import './custom-button.styles.scss';
+import { CustomButtonConatiner } from './custom-button.styles';
 
-const CustomButton=({children,isGoogleSignIn,inverted,...otherProps})=>(
-    <button className={`
-    ${inverted?'inverted':''}
-    ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button `}{...otherProps}>
+const CustomButton=({children,...props})=>(
+    <CustomButtonConatiner {...props}>
         {children}
-    </button>
+    </CustomButtonConatiner>
 )
 
 export default CustomButton;
