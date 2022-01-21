2873efa268bfcf47d17580ea38a7f18db0cda25c
Neha <nehaprajapati1150@gmail.com>
2022-01-21 17:44:43 +0530

    bringing shopdata to app

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
-import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import { Route } from 'react-router-dom';
 import CollectionOverview from '../../components/collections-overview/collections-overview.component'
 import CollectionPage from '../collection/collection.component';
+import { firestore,convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
-const ShopPage=({match})=>(
-<div className='shop-page'>
-<Route exact path={`${match.path}`} component={CollectionOverview}/>
-{/* it actually allows us to access this category ID as a parameter on the match object when we're inside of our category page. */}
-<Route path={`${match.path}/:collectionId`} component={CollectionPage}/>        
-</div>
-    )
+console.log('neha');
+
+class ShopPage extends React.Component{
+    unsubscribeFromSnapshot=null;       //collections array from firestore
+
+componentDidMount(){
+ 
+    const collectionRef=firestore.collection('collections')
+
+//whenever the collection ref updates or whenever this code gets run for the first time, 
+// this collection ref will send us the snapshot representing the code of our collections
+// objects array at the time when this code renders
+collectionRef.onSnapshot(async snapshot=>{
+    convertCollectionsSnapshotToMap(snapshot)
+})
+}
+
+  render(){
+      const {match}=this.props
+      return (
+      <div className='shop-page'>
+      <Route exact path={`${match.path}`} component={CollectionOverview}/>
+      {/* it actually allows us to access this category ID as a parameter on the match object when we're inside of our category page. */}
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>        
+      </div>
+          )
+  }
+}
+
 
 
 
