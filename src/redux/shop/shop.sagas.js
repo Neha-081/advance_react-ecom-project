16b8a3b40db970ac6e19cb8711e831a49887f6a4
Neha <nehaprajapati1150@gmail.com>
2022-01-23 21:52:02 +0530

    redux thunk into saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -2,13 +2,32 @@
 //sagas  wants to run them all together in a way that does not block the execution.
 
 //listens to every action of the specific type
-import {takeEvery} from 'redux-saga/effects';
+import {takeEvery,call,put} from 'redux-saga/effects';
 import ShopActionTypes from './shop.types';
+import { firestore,convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import { fetchCollectionsSuccess,
+fetchCollectionsFailure
+} from './shop.actions';
 
 
 //yield-determine whether or not to cancel any of the previously started sagas from the other actions that came in
-export function* fetchCollectionsAsync(){
-  yield  console.log('i am fired');
+export function* fetchCollectionsAsync(){    //generator function
+ 
+yield  console.log('i am fired');
+
+try{
+  const collectionRef=firestore.collection('collections');
+ //similar to async away in the sense that when we set this new snapshot value equal to the yielded value of running collectionRef.get(). When this value comes back, it comes back in a promise form that gets resolved with the value of our collection reference, which is our snapshot in a constant instead of a callback.
+  const snapshot=yield collectionRef.get()
+
+  //call-t's a method that takes as its first argument, some function or method.And then the subsequent arguments will be the parameters that you passed into this function call, and because we're yielding this call, it allows, again, us to defer control at this point of the execution back to the saga middleware.
+  const collectionMap=yield call(convertCollectionsSnapshotToMap,snapshot)
+  
+  //put-saga effect of creating functions
+  yield put(fetchCollectionsSuccess(collectionMap))
+}catch(error){
+  yield put(fetchCollectionsFailure(error.message))
+}
 }
 
 export function* fetchCollectionStart(){
